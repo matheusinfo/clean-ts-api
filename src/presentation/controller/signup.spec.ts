0a1b732b70f07bd44f1f521318f118ebb749b2e8
MatheusINFO <matheus.add12@gmail.com>
2020-12-13 19:08:15 -0300

    test: ensure SignUpController returns an error if no name is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -12,5 +12,6 @@ describe('SignUp Controlller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
