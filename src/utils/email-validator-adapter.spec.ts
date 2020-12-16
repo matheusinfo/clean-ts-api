71da912a17dd2ed93cc08971ea5855893f1b4d16
MatheusINFO <matheus.add12@gmail.com>
2020-12-15 21:29:44 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,15 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  it('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
+})
