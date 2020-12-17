06d7f24edfad653c7436d95b943f69ccbec649e0
MatheusINFO <matheus.add12@gmail.com>
2020-12-17 20:42:32 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,20 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (uri: string) {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect () {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
