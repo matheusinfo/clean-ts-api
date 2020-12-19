51c75d3635d59d18ccd313c888a382d6b486e7b5
MatheusINFO <matheus.add12@gmail.com>
2020-12-19 09:01:19 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -36,7 +36,7 @@ export class SignUpController implements Controller {
       })
       return success(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
