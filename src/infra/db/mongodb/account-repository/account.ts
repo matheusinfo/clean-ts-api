f06126f91d10b4098d401a20535a568fddd6b9df
MatheusINFO <matheus.add12@gmail.com>
2020-12-19 06:48:04 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helper/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
     return MongoHelper.mapper(account)
--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -2,8 +2,10 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
   async connect (uri: string) {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -12,10 +14,11 @@ export const MongoHelper = {
 
   async disconnect () {
     await this.client.close()
+    this.client = null
   },
 
   async clean (collectionName: string) {
-    const collection = MongoHelper.getCollection(collectionName)
+    const collection = await MongoHelper.getCollection(collectionName)
     await collection.deleteMany({})
   },
 
@@ -24,7 +27,10 @@ export const MongoHelper = {
     return Object.assign({}, collectionWithoutId, { id: _id })
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   }
 }
