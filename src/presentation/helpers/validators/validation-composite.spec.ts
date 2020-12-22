de5da85a7e26fd1ca35488bb8f7af844440f57c9
MatheusINFO <matheus.add12@gmail.com>
2020-12-21 22:45:49 -0300

    test: ensure ValidationComposite not returns an error if validation succeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -40,4 +40,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  it('Should not return if validation succeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate([])
+    expect(error).toBeFalsy()
+  })
 })
