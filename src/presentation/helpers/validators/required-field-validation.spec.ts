64c636cb283a472d3b08ffa01c9390bd2644d807
MatheusINFO <matheus.add12@gmail.com>
2020-12-21 22:24:10 -0300

    test: ensure RequiredFieldValidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_fieldName'))
   })
+
+  it('Should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any' })
+    expect(error).toBeFalsy()
+  })
 })
