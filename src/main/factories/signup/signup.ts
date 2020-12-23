3cb27abc653cbf46ea4730cd994602c01fa8e366
MatheusINFO <matheus.add12@gmail.com>
2020-12-23 17:27:02 -0300

    refactor: mvoe bcrypt-adapter files to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../presentation/protocols/controller'
 import { SignUpController } from '../../../presentation/controller/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { LogControllerDecorator } from '../../decorators/log'
