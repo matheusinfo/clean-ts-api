e3435b7b45fc0f4b9f5fab7992bc955a6294c79b
MatheusINFO <matheus.add12@gmail.com>
2020-12-24 22:05:36 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:12
 WORKDIR /usr/src/clean-ts-api
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 5000
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-ts-api',
   port: process.env.PORT || 5050,
   secret: process.env.SECRET || 'mapx555'
 }
