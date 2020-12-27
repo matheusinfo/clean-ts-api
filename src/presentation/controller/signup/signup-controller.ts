4ad2777da5aaf4eaf195ad21037a6b710440fa00
MatheusINFO <matheus.add12@gmail.com>
2020-12-26 22:58:17 -0300

    feat: ensure SignUpController returns 403 if email already exists

--- src/presentation/controller/signup/signup-controller.ts
+++ src/presentation/controller/signup/signup-controller.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, HttpResponse, Controller, AddAccount, Validation, Authentication } from './signup-controller-protocols'
-import { badRequest, serverError, success } from '../../helpers/http/http-helper'
+import { badRequest, forbidden, serverError, success } from '../../helpers/http/http-helper'
+import { EmailInUseError } from '../../errors'
 
 export class SignUpController implements Controller {
   constructor (
@@ -15,11 +16,14 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { name, email, password } = httpRequest.body
-      await this.addAccount.add({
+      const account = await this.addAccount.add({
         name,
         email,
         password
       })
+      if (!account) {
+        return forbidden(new EmailInUseError())
+      }
       const accessToken = await this.authentication.auth({
         email,
         password
