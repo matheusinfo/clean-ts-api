a0d367360c1df3cc6ca8554a0783b5d76da3d3d0
MatheusINFO <matheus.add12@gmail.com>
2020-12-27 19:14:02 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -25,3 +25,8 @@ export const success = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
 })
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
