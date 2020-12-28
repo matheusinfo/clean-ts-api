625afd9cfcab696ca4bd92b60412e2eff5367e17
MatheusINFO <matheus.add12@gmail.com>
2020-12-28 16:42:38 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,10 +1,17 @@
+import env from '../config/env'
 import request from 'supertest'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 
 describe('Login Routes', () => {
+  let accountCollection: Collection
+
   beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
     await MongoHelper.clean('surveys')
+    await MongoHelper.clean('accounts')
   })
 
   beforeAll(async () => {
@@ -16,7 +23,7 @@ describe('Login Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    it('Should return 403 on add survey withou accessToken', async () => {
+    it('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -29,6 +36,38 @@ describe('Login Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    it('Should return 204 on add survey success', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Matheus',
+        email: 'matheus@gmail.com',
+        password: '1234',
+        passwordConfirmation: '1234',
+        role: 'admin'
+      })
+      const id = result.ops[0]._id
+      const accessToken = sign({ id }, env.secret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
     })
   })
 })
