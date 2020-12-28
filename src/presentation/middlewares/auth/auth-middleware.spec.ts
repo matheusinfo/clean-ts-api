50506471dec4ea1894957ffc80a90f08e23566dd
MatheusINFO <matheus.add12@gmail.com>
2020-12-28 11:19:17 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -1,9 +1,7 @@
 import { AuthMiddleware } from './auth-middleware'
+import { HttpRequest, AccountModel, LoadAccountByToken } from './auth-middleware-protocols'
 import { forbidden, serverError, success } from '../../helpers/http/http-helper'
 import { AccessDeniedError } from '../../errors'
-import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
-import { AccountModel } from '../../../domain/models/account'
-import { HttpRequest } from '../../protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
--- src/presentation/middlewares/auth/auth-middleware.ts
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -1,7 +1,6 @@
-import { HttpRequest, HttpResponse, Middleware } from '../../protocols'
+import { HttpRequest, HttpResponse, Middleware, LoadAccountByToken } from './auth-middleware-protocols'
 import { forbidden, success, serverError } from '../../helpers/http/http-helper'
 import { AccessDeniedError } from '../../errors'
-import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
 
 export class AuthMiddleware implements Middleware {
   constructor (
