a69157ea7c690c5b9087a9b27a9ec0c0116c9213
MatheusINFO <matheus.add12@gmail.com>
2020-12-28 15:48:04 -0300

    feat: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -115,5 +115,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
+
+    it('Should return null if loadbyEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeFalsy()
+    })
   })
 })
