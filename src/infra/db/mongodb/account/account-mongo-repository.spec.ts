03eecf71d83e718dcb092c90a611dc66753d50c6
MatheusINFO <matheus.add12@gmail.com>
2020-12-28 16:58:19 -0300

    refactor: remove duplicated code

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -116,23 +116,6 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    it('Should return null on loadByToken with invalid role', async () => {
-      const sut = makeSut()
-      await accountCollection.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password',
-        accessToken: 'any_token'
-      })
-      const account = await sut.loadByToken('any_token')
-      expect(account).toBeTruthy()
-      expect(account.id).toBeTruthy()
-      expect(account.name).toBe('any_name')
-      expect(account.email).toBe('any_email@mail.com')
-      expect(account.password).toBe('any_password')
-    })
-
     it('Should return an account on loadByToken with user is admin', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
