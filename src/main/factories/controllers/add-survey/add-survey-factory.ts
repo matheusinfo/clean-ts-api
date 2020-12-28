5f7b8f1ac5cfeeacb43d01608e42dcf4e452f7e3
MatheusINFO <matheus.add12@gmail.com>
2020-12-27 21:24:57 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-factory.ts
@@ -0,0 +1,10 @@
+import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log/log-controller-decorator-factory'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { AddSurveyController } from '../../../../presentation/controller/add-survey/add-survey-controller'
+import { makeDbAddSurvey } from '../../usecases/add-survey/db-add-survey-factory'
+
+export const makeAddSurveyController = (): Controller => {
+  const addSurveyController = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
+  return makeLogControllerDecorator(addSurveyController)
+}
---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
+
+describe('Login Routes', () => {
+  beforeEach(async () => {
+    await MongoHelper.clean('surveys')
+  })
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('POST /surveys', () => {
+    it('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
