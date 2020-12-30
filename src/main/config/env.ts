14b765dc2851e139dc01cd0b111c68d467e76f41
MatheusINFO <matheus.add12@gmail.com>
2020-12-30 12:16:39 -0300

    fix: remove docker warnings

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-ts-api',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-ts-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://cleanapi:cleanapi@cluster0.ab3ep.mongodb.net/<dbname>?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   secret: process.env.SECRET || 'mapx555'
 }
