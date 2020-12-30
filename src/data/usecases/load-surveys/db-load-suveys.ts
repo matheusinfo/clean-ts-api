e7f43442942af4afabdb9b85a72a9903915b0502
MatheusINFO <matheus.add12@gmail.com>
2020-12-29 22:17:07 -0300

    feat: ensure DbLoadSurveys return a list of surveys on success

--- src/data/usecases/load-surveys/db-load-suveys.ts
+++ src/data/usecases/load-surveys/db-load-suveys.ts
@@ -8,7 +8,7 @@ export class DbLoadSurveys implements LoadSurveys {
   ) {}
 
   async load (): Promise<SurveyModel[]> {
-    await this.loadSurveysRepository.loadAll()
-    return null
+    const surveys = await this.loadSurveysRepository.loadAll()
+    return surveys
   }
 }
