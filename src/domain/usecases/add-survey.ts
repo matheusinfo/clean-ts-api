cfe1aaf49b6de669b6d0cc22c78536131601fb9c
MatheusINFO <matheus.add12@gmail.com>
2020-12-29 21:31:03 -0300

    feat: ensure LoadSurveysController calls LoadSurvey

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,11 +1,8 @@
-export interface SurveyAnswer {
-  image?: string
-  answer: string
-}
+import { SurveyAnswerModel } from '../models/survey'
 
 export interface AddSurveyModel {
   question: string
-  answers: SurveyAnswer[]
+  answers: SurveyAnswerModel[]
   date: Date
 }
 
---
+++ src/presentation/controller/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,12 @@
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (
+    private readonly loadSurveys: LoadSurveys
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
