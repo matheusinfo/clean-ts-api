d4ffad910b8de33fe023baf45fd825b3e6ce9ac5
MatheusINFO <matheus.add12@gmail.com>
2020-12-29 23:02:11 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -7,9 +7,11 @@ import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 
 describe('Login Routes', () => {
   let accountCollection: Collection
+  let surveyCollection: Collection
 
   beforeEach(async () => {
     accountCollection = await MongoHelper.getCollection('accounts')
+    surveyCollection = await MongoHelper.getCollection('surveys')
     await MongoHelper.clean('surveys')
     await MongoHelper.clean('accounts')
   })
@@ -76,6 +78,60 @@ describe('Login Routes', () => {
       await request(app)
         .get('/api/surveys')
         .expect(403)
+    })
+
+    it('Should return 200 on load surveys success with no content', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Matheus',
+        email: 'matheus@gmail.com',
+        password: '1234',
+        passwordConfirmation: '1234',
+        role: 'admin'
+      })
+      const id = result.ops[0]._id
+      const accessToken = sign({ id }, env.secret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          answer: 'any_answer',
+          image: 'any_image'
+        }],
+        date: new Date()
+      }])
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
+
+    it('Should return 204 on load surveys success with no content', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Matheus',
+        email: 'matheus@gmail.com',
+        password: '1234',
+        passwordConfirmation: '1234',
+        role: 'admin'
+      })
+      const id = result.ops[0]._id
+      const accessToken = sign({ id }, env.secret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(204)
     })
   })
 })
