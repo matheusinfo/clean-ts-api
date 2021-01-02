dee36c6c5f124ed2f8cf06680e535f0d6439d143
MatheusINFO <matheus.add12@gmail.com>
2021-01-02 08:28:06 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,23 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-results'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { MongoHelper } from '../helper/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveys')
+    const result = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    },{
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    return result.value && MongoHelper.mapper(result.value)
+  }
+}
