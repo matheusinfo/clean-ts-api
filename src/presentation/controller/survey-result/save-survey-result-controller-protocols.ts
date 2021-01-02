ef610268bfeb44c54c2915a374a544368c89508f
MatheusINFO <matheus.add12@gmail.com>
2021-01-02 10:00:24 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controller/survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controller/survey-result/save-survey-result-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from '@/presentation/protocols'
 export * from '@/domain/usecases/survey/load-survey-by-id'
-export * from '../load-surveys/load-surveys-protocols'
+export * from '@/presentation/controller/load-surveys/load-surveys-protocols'
+export * from '@/domain/usecases/survey-result/save-survey-result'
+export * from '@/domain/models/survey-results'
--- src/presentation/controller/survey-result/save-survey-result-controller.ts
+++ src/presentation/controller/survey-result/save-survey-result-controller.ts
@@ -1,15 +1,16 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult } from './save-survey-result-controller-protocols'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements Controller {
   constructor (
-    private readonly loadSurveyById: LoadSurveyById
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { params, body } = httpRequest
+      const { params, body, accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(params.surveyId)
       if (survey) {
         const answers = survey.answers.map(answer => answer.answer)
@@ -19,6 +20,12 @@ export class SaveSurveyResultController implements Controller {
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
+      await this.saveSurveyResult.save({
+        surveyId: params.surveyId,
+        accountId,
+        answer: body.answer,
+        date: new Date()
+      })
       return null
     } catch (error) {
       return serverError(error)
