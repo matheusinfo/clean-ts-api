0fadf174f41f53dcaaa3f241e4163f3ec2e4de0a
MatheusINFO <matheus.add12@gmail.com>
2021-01-05 22:41:17 -0300

    docs: update save survey result docs

--- src/main/docs/schemas.ts
+++ src/main/docs/schemas.ts
@@ -8,7 +8,8 @@ import {
   signupParamsSchema,
   addSurveyParamsParamsSchema,
   saveSurveyParamsSchema,
-  surveyResultSchema
+  surveyResultSchema,
+  surveyResultAnswerSchema
 } from './schemas/'
 
 export default {
@@ -21,5 +22,6 @@ export default {
   surveys: surveysSchema,
   addSurveyParams: addSurveyParamsParamsSchema,
   saveSurveyParams: saveSurveyParamsSchema,
-  surveyResult: surveyResultSchema
+  surveyResult: surveyResultSchema,
+  surveyResultAnswer: surveyResultAnswerSchema
 }
