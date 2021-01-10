acdbc71b629725d2dc4d589da9ececdf885d8174
MatheusINFO <matheus.add12@gmail.com>
2021-01-09 23:03:30 -0300

    feat: change load surveys to add didAnswer field

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,4 +1,3 @@
-
 import {
   Authentication,
   LoadAccountByEmailRepository,
--- src/presentation/controller/load-surveys/load-surveys-controller.ts
+++ src/presentation/controller/load-surveys/load-surveys-controller.ts
@@ -8,7 +8,7 @@ export class LoadSurveysController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const surveys = await this.loadSurveys.load()
+      const surveys = await this.loadSurveys.load(httpRequest.accountId)
       return surveys.length ? success(surveys) : noContent()
     } catch (error) {
       return serverError(error)
