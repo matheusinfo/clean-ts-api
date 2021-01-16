21a861ad4191129755775816eefd5524c73515d5
MatheusINFO <matheus.add12@gmail.com>
2021-01-16 16:45:58 -0300

    test: ensure SaveSurveyResult Mutation returns AccessDeniedError if no token is provided

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -183,5 +183,28 @@ describe('Survey Result GraphQL', () => {
         isCurrentAccountAnswer: false
       }])
     })
+
+    it('Should return AccessDeniedError if no token is provided', async () => {
+      const now = new Date()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(saveSurveyResultMutation, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString(),
+          answer: 'Answer 1'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Access denied')
+    })
   })
 })
